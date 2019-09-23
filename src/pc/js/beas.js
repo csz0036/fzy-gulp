cbca42edbb0974ab9be2bd12da96f6594f831814
chenshuizhang <chenshuizhang@oneship.cc>
2019-09-23 10:31:01 +0800

    悬浮

--- src/pc/js/beas.js
+++ src/pc/js/beas.js
@@ -4,4 +4,12 @@ $(function () {
     }, function () {
         $("#productNavList").hide()
     })
+    //为返回顶部元素添加点击事件
+    $('#return_top').click(function () {
+        //将当前窗口的内容区滚动高度改为0，即顶部
+        $("html,body").animate({
+            scrollTop: 0
+        }, "fast");
+    });
+
 })
