b893da7913db05f52d7ff235de879ba203d20d75
chenshuizhang <chenshuizhang@oneship.cc>
2019-10-08 18:19:02 +0800

    nav

--- src/m/js/basis.js
+++ src/m/js/basis.js
@@ -1,12 +1,13 @@
 $(function () {
   // 头部操作
   $("#head .button").on('click', function () {
-    var display = $('#head .navListButton').css('display');
-    if (display == 'none') {
-      $("#head .navListButton").show()
-    } else {
-      $("#head .navListButton").hide()
-    }
+    $(this).toggleClass('activeNav')
+    $("#head .navListButton").toggleClass('activeNav')
+
+  });
+  $(".navListButton").on('click', 'li', function () {
+    $(this).addClass('openList').siblings('li').removeClass('openList');
+
   })
 
   //获取URL参数
